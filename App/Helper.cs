ec43af0ccae47c698d4cc751580811ea202d1ed6
Serhii Moshtakov <serhii.moshtakov@students.fhnw.ch>
2023-12-07 22:23:30 +0100

    new method added

--- App/Helper.cs
+++ App/Helper.cs
@@ -3,12 +3,62 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection.Metadata.Ecma335;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace App
 {
     public class Helper
     {
+        // method that changes <, >, & to &lt;, &gt;, &amp;
+        public String UnitTest(string input, Dictionary<string, string>? map = null)
+        {
+            if (input == null) throw new ArgumentNullException("HTML should not be null"); // checks if input is not null
+            if (input.Length == 0) // checks in input string is not empty
+            {
+                throw new ArgumentException("HTML must not be empty");
+            }
+            if (!IsHtmlValid(input)) // checks if there is a valid html string
+            {
+                throw new ArgumentException("This should be a correct html file");
+            }
+            Dictionary<string, string> htmlSpecSymbols = map ?? new Dictionary<string, string> { { "&", "&amp;" }, { "<", "&lt;" }, { ">", "&gt;" } };
+            var escapedHtml = new StringBuilder();
+            foreach (var c in input)
+                escapedHtml.Append(htmlSpecSymbols.TryGetValue(c.ToString(), out var replacement) ? replacement : c.ToString());
+
+            return escapedHtml.ToString();
+        }
+        // checks if there is a attributes in the html tag
+        public bool ContainsAttributes(String html)
+        {
+            string pattern = @"\w+\s+[^=]*(\w+=[^>]+)+>";
+            Regex regex = new Regex(pattern, RegexOptions.IgnoreCase);
+            return regex.IsMatch(html);
+        }
+        // checks if it is a valid html string
+        private bool IsHtmlValid(string input)
+        {
+            int openTagCount = 0;
+            foreach (char c in input)
+            {
+                if (c == '<')
+                {
+                    openTagCount++;
+                }
+                else if (c == '>')
+                {
+                    if (openTagCount == 0)
+                    {
+                        return false;
+                    }
+                    openTagCount--;
+                }
+            }
+
+            return openTagCount == 0;
+        }
+
         public String Ellipsis(string input, int len)
         {
             if (input == null) throw new ArgumentNullException("Null detected of parameter: " + nameof(input)); //check if null parameter
--- UnitTest/UnitTest1.cs
+++ UnitTest/UnitTest1.cs
@@ -5,6 +5,72 @@ namespace UnitTest
     [TestClass]
     public class HelperTest
     {
+        [TestMethod] // check function ContainsAttributes() with different cases
+        public void ContainsContainsAttributesTest()
+        {
+            Helper helper = new Helper();
+            Assert.IsNotNull(helper, "new Helper() should not be null");
+            Assert.IsTrue(helper.ContainsAttributes("<div style=\"\"></div>"));
+            Assert.IsTrue(helper.ContainsAttributes("<i style=\"code\" ></div>"));
+            Assert.IsTrue(helper.ContainsAttributes("<i style=\"code\"  required ></div>"));
+            Assert.IsTrue(helper.ContainsAttributes("<i style='code'  required></div>"));
+            Assert.IsTrue(helper.ContainsAttributes("<i required style=\"code\" ></div>"));
+            Assert.IsTrue(helper.ContainsAttributes("<i required style=\"code\"></div>"));
+            Assert.IsTrue(helper.ContainsAttributes("<img onload=\"dangerCode()\" src=\"puc.png\"/>"));
+            Assert.IsTrue(helper.ContainsAttributes("<img  src=\"images/img.png\"/>"));
+            Assert.IsTrue(helper.ContainsAttributes("<img width=100 />"));
+            Assert.IsFalse(helper.ContainsAttributes("<div></div>"));
+            Assert.IsFalse(helper.ContainsAttributes("<div ></div>"));
+            Assert.IsFalse(helper.ContainsAttributes("<br/>"));
+            Assert.IsFalse(helper.ContainsAttributes("<br />"));
+            Assert.IsFalse(helper.ContainsAttributes("<div requred ></div>"));
+            Assert.IsFalse(helper.ContainsAttributes("<div required ></div>"));
+            Assert.IsFalse(helper.ContainsAttributes("<div required></div>"));
+        }
+
+        [TestMethod] // testing function EscapeHtml() with different cases
+        public void TestEscapeHtml()
+        {
+            Helper helper = new Helper();
+            Assert.IsNotNull(helper, "new Helper() should not be null");
+            Dictionary<String, String> testCases = new()
+            {
+                {"<p>Hello World!</p>", "&lt;p&gt;Hello World!&lt;/p&gt;"},
+                {"<img scr='images/image.jpg'>", "&lt;img scr='images/image.jpg'&gt;"},
+            };
+            foreach (var testCase in testCases)
+            {
+                Assert.AreEqual(
+                    testCase.Value,
+                    helper.EscapeHtml(testCase.Key),
+                    $"{testCase.Value} -- {testCase.Key[0]} + {testCase.Key[1]}"
+                    );
+            }
+        }
+
+        [TestMethod] // testing cases where are exception in method EscapeHtml()
+        public void TestEscapeHtmlEx()
+        {
+            Helper helper = new Helper();
+            Assert.IsNotNull(helper, "new Helper() should not be null");
+            var ex = Assert.ThrowsException<ArgumentNullException>(
+                () => helper.EscapeHtml(null!)
+                );
+            Assert.IsTrue(ex.Message.Contains("HTML"), "HTML should not be null");
+            var ex1 = Assert.ThrowsException<ArgumentException>(
+                () => helper.EscapeHtml("<<<<html>ndjngjdn</html>")
+                );
+            Assert.IsTrue(ex1.Message.Contains("correct"), "This should be a correct html file");
+            var ex2 = Assert.ThrowsException<ArgumentException>(
+                () => helper.EscapeHtml("p>dvd<////p>>>>")
+                );
+            Assert.IsTrue(ex2.Message.Contains("correct"), "This should be a correct html file");
+            var ex3 = Assert.ThrowsException<ArgumentException>(
+                () => helper.EscapeHtml("")
+                );
+            Assert.IsTrue(ex3.Message.Contains("empty"), "HTML must not be empty");
+        }
+
         // general testing of a method Ellipsis()
         [TestMethod]
         public void EllipsisTest()
